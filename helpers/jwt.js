58ac1fde49b3bf55b8924bcd0708e56971a1afe2
Abdel-dev1 <84016355+Abdel-dev1@users.noreply.github.com>
2021-10-10 20:48:55 +0200

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -12,7 +12,6 @@ function authJwt() {
         path: [
             {url:/\/api\/v1\/products(.*)/, methods:['GET','OPTIONS'] },
              {url:/\/api\/v1\/categories(.*)/, methods:['GET','OPTIONS'] },
-             {url:/\/api\/v1\/users/register(.*)/, methods:['POST','OPTIONS'] },
             api+'/users/login',
             api+'/users/register',
        
