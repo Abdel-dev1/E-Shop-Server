b1ead6c3440bb2b8b0c93f19f25d7c2b7fa46b8a
Abdel-dev1 <najum1993@gmail.com>
2022-09-04 19:53:44 +0100

    socket

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const categoryRouter = require("./routers/categories");
 const orderRouter = require("./routers/orders");
 const conversationRouter = require("./routers/conversations");
 const messageRouter = require("./routers/messages");
+const { UnauthorizedError } = require("express-jwt/lib");
 
 const api = process.env.API_URL;
 
@@ -68,12 +69,20 @@ let users=[];
 const addUser=(userId,socketId)=>{
   !users.some(user=>user.userId===userId) && users.push({userId,socketId})
 }
+const removeUser=(socketId)=>{
+users=users.filter(user=>user.socketId!==socketId)
+}
 
 io.on("connection", (socket) => {
   console.log("/*******************  a user is connected **********************/");
   //io.emit("Welcome","hello this is socket")
   socket.on("addUser",userId=>{
     addUser(userId,socket.id)
+    io.emit("getUsers",users)
+  })
+
+  socket.on("disconnect",()=>{
+    removeUser(socket.id)
     io.emit("getUsers",users)
   })
 });
