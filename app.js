cfa33907822c0de77fd8401d355de64e9888aba5
Abdel-dev1 <najum1993@gmail.com>
2022-09-04 18:58:38 +0100

    socket

--- app.js
+++ app.js
@@ -1,57 +1,52 @@
-const express=require('express');
-const app=express();
-const bodyParser= require('body-parser');
-const morgan=require('morgan');
-const mongoose=require('mongoose');
-require('dotenv/config');
-var authJwt=require('./helpers/jwt');
-const errorHandler=require('./helpers/error-Handler');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv/config");
+var authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-Handler");
 
-const secret=process.env.secret;
+const secret = process.env.secret;
 
 //************middleware************
 
 app.use(bodyParser.json());
-app.use(morgan('tiny'));  //display log format http
-app.use(authJwt());  //{secret:secret, algorithms:['RS256','HS256']})
+app.use(morgan("tiny")); //display log format http
+app.use(authJwt()); //{secret:secret, algorithms:['RS256','HS256']})
 app.use(errorHandler);
 
-
 //**********routers*****************
-const productsRouter=require('./routers/products');
-const usersRouter=require('./routers/users');
-const categoryRouter=require('./routers/categories');
-const orderRouter=require('./routers/orders');
-const conversationRouter=require('./routers/conversations');
-const messageRouter=require('./routers/messages');
+const productsRouter = require("./routers/products");
+const usersRouter = require("./routers/users");
+const categoryRouter = require("./routers/categories");
+const orderRouter = require("./routers/orders");
+const conversationRouter = require("./routers/conversations");
+const messageRouter = require("./routers/messages");
 
+const api = process.env.API_URL;
 
-
-
-const api= process.env.API_URL;
-
-app.use(api+'/products',productsRouter);
-app.use(api+'/users',usersRouter);
-app.use(api+'/categories',categoryRouter);
-app.use(api+'/orders',orderRouter);
-app.use(api+'/conversations',conversationRouter);
-app.use(api+'/messages',messageRouter);
-
-
+app.use(api + "/products", productsRouter);
+app.use(api + "/users", usersRouter);
+app.use(api + "/categories", categoryRouter);
+app.use(api + "/orders", orderRouter);
+app.use(api + "/conversations", conversationRouter);
+app.use(api + "/messages", messageRouter);
 
 //connection to DB
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName:'E-shopDB'
-})
-.then(()=>{
-    console.log('DB Connected...');
-})
-.catch((err)=>{
+    dbName: "E-shopDB",
+  })
+  .then(() => {
+    console.log("DB Connected...");
+  })
+  .catch((err) => {
     console.log(err);
-    console.log('err');
-})
+    console.log("err");
+  });
 
 // server
 // server
@@ -59,20 +54,23 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log('server is runinnig on http://localhost:3000');
 })  */
 //const { createServer, Server } = require("http");
-//const httpServer=require("http").createServer(app);
-//const io= require("socket.io")(httpServer);
+const httpServer = require("http").Server(app);
+httpServer.listen(process.env.PORT || 3000, function () {
+  var port = this.address().port;
+  console.log("Express is working on port: " + port);
+});
+const io = require("socket.io")(httpServer, {
+  //....
+});
 // Socket io
-/* 
-io.on("connection", (socket)=>{
-    console.log("a user connected.");
-}) */
+
+io.on("connection", (socket) => {
+  console.log("a user is connected");
+  io.emit("Welcome","hello this is socket")
+});
 //var port = server.address().port;
-//httpServer.listen(process.env.PORT || 3000);
 
-
-
- var server = app.listen(process.env.PORT, function(){
+/*   var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
     console.log("Express is working on port: " + port)
-})
- 
+})  */
